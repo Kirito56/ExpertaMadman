8624d494f6538a6948f87bcaf8216abc0051df6b
MadMan <mindmad34@gmail.com>
2021-11-17 11:34:03 +0200

    THB

--- Asserts.py
+++ Asserts.py
@@ -12,8 +12,8 @@ class Asserts(KnowledgeEngine):
                                   price=3000,
                                   producer='nVidia'))
         self.declare(graphic_card(name='Gigabyte_PCI-Ex_GeForce_GT_710',
-                                  functions=['DirectX_12' ,'OpenGL_4.5'],
-                                  inputs=['DVI' ,'VGA', 'HDMI'],
+                                  functions=['DirectX_12', 'OpenGL_4.5'],
+                                  inputs=['DVI', 'VGA', 'HDMI'],
                                   memory=2,
                                   purpose='pro',
                                   price=5000,
