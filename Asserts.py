67ee32427b3be579f23be372d147206d59919a8b
MadMan <mindmad34@gmail.com>
2021-11-17 11:12:49 +0200

    Update Asserts.py

--- Asserts.py
+++ Asserts.py
@@ -1,1 +1,20 @@
+from ES import graphic_card
 from experta.engine import KnowledgeEngine
+
+
+class Asserts(KnowledgeEngine):
+    def init_graphic_card(self):
+        self.declare(graphic_card(name='MSI_PCI-Ex_GeForce_GT_730',
+                                  functions='DirectX_12 OpenGL_4.4',
+                                  inputs='DVI VGA HDMI',
+                                  memory='2',
+                                  purpose='office',
+                                  price='3000',
+                                  producer='nVidia'))
+        self.declare(graphic_card(name='Gigabyte_PCI-Ex_GeForce_GT_710',
+                                  functions='DirectX_12 OpenGL_4.5',
+                                  inputs='DVI VGA HDMI',
+                                  memory='2',
+                                  purpose='pro',
+                                  price='5000',
+                                  producer='nVidia'))
