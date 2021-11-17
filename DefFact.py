69bfc691b70e7ec04d164f1c3d8410056c1dc8e8
SkillFi <bimbom2345@gmail.com>
2021-11-17 12:25:41 +0200

    added imports MATCH, TEST

--- DefFact.py
+++ DefFact.py
@@ -1,6 +1,8 @@
 from experta.engine import KnowledgeEngine
 from experta.rule import Rule
 from experta.deffacts import DefFacts
+from experta.shortcuts import MATCH
+from experta.conditionalelement import TEST
 
 from ES import graphic_card
 
